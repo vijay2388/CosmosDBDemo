86417e07ad4d769f9f35722d9a95a6ca526be761
vijay <vijay@DESKTOP-O9BHK5B>
2021-08-13 08:32:58 +0530

    Add project files.

---
+++ CosmosDBDemo/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmosDBDemo
+{
+    class Course
+    {
+        public string id { get; set; }
+        public string courseid { get; set; }
+        public string coursename { get; set; }
+        public decimal rating { get; set; }
+    }
+}
